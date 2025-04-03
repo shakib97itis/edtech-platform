f498208150fc464c42e06153d7ae8d199e8e2ea9
Shakibul Islam <shakib97itis@gmail.com>
2025-04-03 22:52:27 +0600

    add HeadingSecondary component for improved heading styling

---
+++ src/components/common/headings/HeadingSecondary.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function HeadingSecondary({ className, children }) {
+  return (
+    <div className={`${className} relative z-10 text-center`}>
+      <h2 className="font-primary inline-block text-3xl font-bold md:text-4xl">
+        <div className="">{children}</div>
+        <div className="bg-accent-500 inline-block h-[4px] w-[70%]"></div>
+      </h2>
+    </div>
+  );
+}
+
+export default HeadingSecondary;
