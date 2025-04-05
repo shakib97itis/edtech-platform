4a4d6ec470452af16c3226d1af747faa08b231a0
Shakibul Islam <shakib97itis@gmail.com>
2025-04-06 04:49:38 +0600

    Refactor Button component: streamline prop types, update variant styles, and enhance documentation

--- src/components/common/button/Button.jsx
+++ src/components/common/button/Button.jsx
@@ -4,42 +4,24 @@ import PropTypes from "prop-types";
 /**
  * Button component
  * guide to use:
- * - variant: btnOne, btnTwo, btnThree, btnFour, btnFive
+ * - Button variant: btnOne, btnTwo, btnThree, btnFour
  * - className: custom class name
  * - text: button text
- * - Icon: icon component
- * - children: children component
- *
- * @component
- *
- * @example
- * <Button variant={"btnOne"} text={"Sign In"}>
- *  <LuUserRoundPlus className="text-inherit" />
- * </Button>
- *
- * @param {text} text - Button text
- * @param {variant} variant - Button variant
- * @param {className} className - Button class name
- * @param {children} children - Button icon.
- *
- * @returns {JSX.Element} Button component
+ * - children: Icon svg / React-icons, (Any JSX Element)
  */
 
 function Button({ children, text, variant, className }) {
   const variantStyles = {
     btnOne:
-      "bg-accent-500 hover:border-accent-600 hover:bg-accent-600 border-accent-500 border-2 px-4 py-2 text-white",
+      "bg-accent-500 hover:border-accent-600 hover:bg-accent-600 border-accent-500 mb-1 border-2 text-white",
+    // Border width should be provided by classname.
     btnTwo:
-      "border-accent-500 text-accent-500 border hover:border-transparent hover:bg-[#FDE6E0]",
+      "border-accent-500 text-accent-500 hover:bg-accent-400 hover:border-accent-400 border bg-transparent",
     btnThree:
-      "border-accent-500 text-accent-500 hover:text-accent-600 border-2 bg-transparent  hover:border-[#FDE6E0] hover:bg-[#FDE6E0]",
+      "bg-primary-800 border-primary-800 hover:bg-secondary-400 hover:border-secondary-400 border-2 text-white",
     btnFour:
-      "border-2 border-transparent bg-[#004464]  text-white hover:bg-[#29BDBF]",
-    btnFive:
-      "border-2 border-transparent text-[#004464] hover:border-[#B0C5CF] hover:bg-[#36BBE4] hover:text-white",
+      "text-primary-800 hover:bg-primary-300 hover:border-secondary-200 border-2 border-transparent hover:text-white",
   };
-
-  // const baseStyles = "";
 
   return (
     <button
@@ -50,18 +32,5 @@ function Button({ children, text, variant, className }) {
     </button>
   );
 }
-
-Button.propTypes = {
-  text: PropTypes.string,
-  variant: PropTypes.oneOf([
-    "btnOne",
-    "btnTwo",
-    "btnThree",
-    "btnFour",
-    "btnFive",
-  ]),
-  className: PropTypes.string,
-  children: PropTypes.node,
-};
 
 export default Button;
