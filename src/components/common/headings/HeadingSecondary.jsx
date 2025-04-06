b3e4e9fac0abb0c3c79155221bc893a57082af31
Shakibul Islam <shakib97itis@gmail.com>
2025-04-06 13:14:05 +0600

    Fix className order in HeadingSecondary component for improved styling consistency

--- src/components/common/headings/HeadingSecondary.jsx
+++ src/components/common/headings/HeadingSecondary.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function HeadingSecondary({ className, children }) {
   return (
-    <div className={`${className} relative z-10 text-center`}>
+    <div className={`relative z-10 text-center ${className}`}>
       <h2 className="font-primary inline-block text-3xl font-bold md:text-4xl">
         <div className="">{children}</div>
         <div className="bg-accent-500 inline-block h-[4px] w-[70%]"></div>
