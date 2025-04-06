ce488d3842f00915ec5fabb4f30614469dd25460
Shakibul Islam <shakib97itis@gmail.com>
2025-04-06 19:45:39 +0600

    Fix spacing issue in Hero component: adjust className for consistent gap handling in responsive layout

--- src/components/sections/hero/Hero.jsx
+++ src/components/sections/hero/Hero.jsx
@@ -15,7 +15,7 @@ function Hero() {
       </section>
       {/* ===== HERO CARDS ===== */}
       <section>
-        <div className="relative z-10 container mx-auto -mt-[50px] grid grid-cols-1 gap-y-[80px] px-[16px] md:-mt-[150px] md:grid-cols-2 md:px-[40px] lg:grid-cols-3 md:gap-x-[16px]">
+        <div className="relative z-10 container mx-auto -mt-[50px] grid grid-cols-1 gap-y-[80px] px-[16px] md:-mt-[150px] md:grid-cols-2 md:gap-x-[16px] md:px-[40px] lg:grid-cols-3">
           <HeroCard1 />
           <HeroCard1 />
           <HeroCard1 />
