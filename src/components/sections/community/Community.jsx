e58c93cecdd1eef32447d889c26d9c81f75c58b1
Shakibul Islam <shakib97itis@gmail.com>
2025-04-07 21:24:34 +0600

    Refactor CommunitySection: replace static heading with HeadingSecondaryLeft component and update background image

--- src/components/sections/community/Community.jsx
+++ src/components/sections/community/Community.jsx
@@ -1,3 +1,4 @@
+import HeadingSecondaryLeft from "../../common/headings/HeadingSecondaryLeft";
 import CommunityCard from "./CommunityCard";
 
 const CommunitySection = () => {
@@ -6,26 +7,23 @@ const CommunitySection = () => {
       <div
         className="absolute inset-0 bg-cover bg-center bg-no-repeat"
         style={{
-          backgroundImage: "url('/OCB.jpg')",
+          backgroundImage: "url('office-photo.jpg')",
           opacity: 0.2,
           backgroundAttachment: "fixed",
         }}
       ></div>
       <div className="relative z-10 container mx-auto px-[24px] md:px-[40px]">
         <div className="mb-[50px]">
-          <div className="mb-[50px]">
-            <h3 className="text-3xl mb-4 font-bold">Our Community</h3>
-            <div className="h-1 w-[100px] bg-accent-500"></div>
-          </div>
+          <HeadingSecondaryLeft className="mb-[50px]">Our Community</HeadingSecondaryLeft>
 
-          <p className="max-w-2xl text-[#DEDEDE]">
+          <p className="text-neutral-350 max-w-2xl">
             Bulipe Tech aims to train unemployed individuals in Bangladesh,
             equipping them with essential digital skills and linking them to job
             opportunities across 23 countries worldwide.
           </p>
         </div>
 
-        <div className="grid grid-cols-1 gap-3 shadow sm:grid-cols-3 md:gap-0">
+        <div className="grid gap-3 shadow md:grid-cols-3 md:gap-0">
           <CommunityCard number="23+" text="Countries and adding" />
           <CommunityCard number="300+" text="Local training partners" />
           <CommunityCard number="200+" text="Job opportunities" />
