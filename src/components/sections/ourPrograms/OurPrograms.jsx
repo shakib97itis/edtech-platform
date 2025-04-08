485445f04f0a169064aafbc7c9d48d9a262831cc
Shakibul Islam <shakib97itis@gmail.com>
2025-04-08 22:24:00 +0600

    Refactor OurPrograms and ProgramCard components:
    update image paths and enhance layout for better presentation

--- src/components/sections/ourPrograms/OurPrograms.jsx
+++ src/components/sections/ourPrograms/OurPrograms.jsx
@@ -6,42 +6,42 @@ function OurPrograms() {
   // Dummy data for the program cards
   const programs = [
     {
-      img: "/course1.png",
+      img: "/ourProgramsImages/course01.png",
       title: "IT Support Specialist",
       price: 100,
       rating: 4,
       ratingCount: 50,
     },
     {
-      img: "/course1.png",
+      img: "/ourProgramsImages/course02.png",
       title: "Web Development",
       price: 200,
       rating: 5,
       ratingCount: 100,
     },
     {
-      img: "/course1.png",
+      img: "/ourProgramsImages/course03.png",
       title: "Data Science",
       price: 300,
       rating: 3,
       ratingCount: 30,
     },
     {
-      img: "/course1.png",
+      img: "/ourProgramsImages/course04.png",
       title: "Cyber Security",
       price: 400,
       rating: 4,
       ratingCount: 80,
     },
     {
-      img: "/course1.png",
+      img: "/ourProgramsImages/course05.png",
       title: "Digital Marketing",
       price: 500,
       rating: 5,
       ratingCount: 120,
     },
     {
-      img: "/course1.png",
+      img: "/ourProgramsImages/course06.png",
       title: "Graphic Design",
       price: 600,
       rating: 2,
@@ -52,20 +52,19 @@ function OurPrograms() {
     <section className="py-[130px]">
       <div className="container mx-auto px-[16px] md:px-[40px]">
         {/* SECTION TITLE */}
-        <div className={`mb-[55px] text-center`}>
+        <div className="mb-[55px]">
           <HeadingSecondary>Our Programs</HeadingSecondary>
         </div>
         <div className="grid gap-[10px] md:grid-cols-2 lg:grid-cols-3">
           {programs.map((program, index) => (
-            <div key={index} className="mb-[20px]">
-              <ProgramCard
-                img={program.img}
-                title={program.title}
-                price={program.price}
-                rating={program.rating}
-                ratingCount={program.ratingCount}
-              />
-            </div>
+            <ProgramCard
+              key={index}
+              img={program.img}
+              title={program.title}
+              price={program.price}
+              rating={program.rating}
+              ratingCount={program.ratingCount}
+            />
           ))}
         </div>
       </div>
--- src/components/sections/ourPrograms/ProgramCard.jsx
+++ src/components/sections/ourPrograms/ProgramCard.jsx
@@ -5,19 +5,19 @@ import Rating from "./Rating";
 
 const ProgramCard = ({ img, title, price, rating, ratingCount }) => {
   return (
-    <div className="w-full overflow-hidden rounded-2xl border border-gray-200 bg-white p-6 shadow-lg">
+    <div className="w-full overflow-hidden rounded-[12px] border border-gray-200 bg-white p-[24px] shadow-lg">
       {/* Image Section */}
-      <div className="relative overflow-hidden rounded-t-2xl">
+      <div className="mb-[40px] overflow-hidden rounded-t-[12px]">
         <img
           src={img} // Replace with your actual image URL
           alt="IT Support Specialist"
-          className="h-full w-full bg-cover object-cover grayscale transition-all duration-300 hover:grayscale-0"
+          className="h-full w-full object-cover grayscale transition-all duration-300 hover:grayscale-0"
         />
       </div>
 
       {/* Content Section */}
-      <div className="p-[16px]">
-        <h3 className="mb-[16px] text-lg font-semibold text-gray-900">
+      <div>
+        <h3 className="mb-[12px] text-[18px] font-semibold text-gray-900">
           {title}
         </h3>
 
@@ -26,23 +26,23 @@ const ProgramCard = ({ img, title, price, rating, ratingCount }) => {
           <Rating rating={rating} ratingCount={ratingCount} />
 
           {/* Price */}
-          <p className="text-sm text-gray-700">৳ {price}/-</p>
+          <p className="text-sm text-neutral-650">৳ {price}/-</p>
         </div>
 
         {/* Action Buttons */}
-        <div className="flex flex-col items-center justify-between gap-[20px] sm:w-auto sm:flex-row sm:gap-[8px] md:flex-col md:gap-[20px] xl:flex-row xl:gap-[8px]">
+        <div className="flex flex-col items-center justify-between gap-[20px] sm:flex-row sm:gap-[8px] md:flex-col md:gap-[20px] xl:flex-row xl:gap-[8px]">
           <Button
             variant="btnFour"
             text="Read Details"
-            className="w-full px-[10px] py-[5px] text-sm"
+            className="w-full px-[10px] py-[5px] text-sm sm:w-auto md:w-full xl:w-auto"
           >
             <FaArrowCircleRight />
           </Button>
 
           <Button
-            variant="btnThree"
+            variant="btnTwo"
             text="Enroll Now"
-            className="w-full px-[30px] py-[8px] text-sm"
+            className="w-full px-[30px] py-[5px] text-sm sm:w-auto md:w-full xl:w-auto"
           ></Button>
         </div>
       </div>
