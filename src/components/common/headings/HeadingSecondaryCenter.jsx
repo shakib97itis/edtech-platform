e044b5e8b023b50f291a4243c2f8c09715e400f5
Shakibul Islam <shakib97itis@gmail.com>
2025-04-08 15:44:53 +0600

    Refactor HeadingSecondary components:
    update typography and spacing for consistency and improved readability

--- src/components/common/headings/HeadingSecondaryCenter.jsx
+++ src/components/common/headings/HeadingSecondaryCenter.jsx
@@ -3,9 +3,9 @@ import React from "react";
 function HeadingSecondary({ className, children }) {
   return (
     <div className={`relative z-10 text-center ${className}`}>
-      <h2 className="font-primary inline-block text-3xl font-bold md:text-4xl">
-        <div className="">{children}</div>
-        <div className="bg-accent-500 inline-block h-[4px] w-[70%]"></div>
+      <h2 className="font-primary inline-block text-[30px] leading-none font-bold md:text-[36px]">
+        <div className="text-inherit">{children}</div>
+        <div className="bg-accent-500 inline-block h-[2px] w-[70%]"></div>
       </h2>
     </div>
   );
--- src/components/common/headings/HeadingSecondaryLeft.jsx
+++ src/components/common/headings/HeadingSecondaryLeft.jsx
@@ -3,9 +3,9 @@ import React from "react";
 function HeadingSecondaryLeft({ children, className }) {
   return (
     <div className={className}>
-      <h2 className="inline-block space-y-3 text-3xl font-bold">
+      <h2 className="inline-block space-y-3 text-[30px] leading-none font-bold md:text-[36px]">
         <div className="text-inherit">{children}</div>
-        <div className="bg-accent-500 h-1 w-[70%]"></div>
+        <div className="bg-accent-500 h-[2px] w-[70%]"></div>
       </h2>
     </div>
   );
