de9a4dabb3e5599ea44b6536cc075734686da7eb
Shakibul Islam <shakib97itis@gmail.com>
2025-04-09 00:29:00 +0600

    Refactor NavButton component:
    Remove unused onClick and ref props for cleaner code

--- src/components/common/button/NavButton.jsx
+++ src/components/common/button/NavButton.jsx
@@ -1,13 +1,8 @@
 import { LuChevronRight, LuChevronLeft } from "react-icons/lu";
 
-function NavButton({ direction = "left", onClick, ref }) {
+function NavButton({ direction = "left" }) {
   return (
-    <button
-      onClick={onClick}
-      ref={ref}
-      
-      className="text-primary-700 border-primary-700 hover:bg-secondary-300 flex h-10 w-10 cursor-pointer items-center justify-center rounded-full border-2 transition-colors"
-    >
+    <button className="text-primary-700 border-primary-700 hover:bg-secondary-300 flex h-10 w-10 cursor-pointer items-center justify-center rounded-full border-2 transition-colors">
       {direction === "left" ? (
         <LuChevronLeft className="text-inherit" />
       ) : (
